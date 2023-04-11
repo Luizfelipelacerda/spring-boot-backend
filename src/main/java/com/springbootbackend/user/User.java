733626ccd5abddca942810015a59f334f92c2c4f
Luiz Lacerda <luizfelipe_lacerda@ymail.com>
2023-04-11 17:11:06 -0300

    first commit

---
+++ src/main/java/com/springbootbackend/user/User.java
@@ -0,0 +1,34 @@
+package com.springbootbackend.user;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Entity
+@Table
+public class User {
+
+    @Id
+    @SequenceGenerator(
+            name = "user_sequence",
+            sequenceName = "user_sequence",
+            allocationSize = 1
+    )
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "user_sequence"
+    )
+    private Long id;
+    private String name;
+    private String email;
+    private Integer age;
+
+
+
+}
---
+++ src/main/java/com/springbootbackend/user/UserRepository.java
@@ -0,0 +1,15 @@
+package com.springbootbackend.user;
+
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository
+        extends JpaRepository<User, Long> {
+
+
+
+
+
+}
